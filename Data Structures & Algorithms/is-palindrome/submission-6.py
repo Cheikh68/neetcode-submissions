142456be8ecf707a88e240633be4fe6c0ee189bc
Cheikh68 <sallcheikh222@gmail.com>
2026-09-10 17:11:10 -0400

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        word = "".join(char.lower() for char in s if char.isalnum())
+        i = 0
+        j = len(word) - 1
+
+        while i < j:
+            if word[i] != word[j]:
+                return False
+            i += 1
+            j -= 1
+        
+        return True
