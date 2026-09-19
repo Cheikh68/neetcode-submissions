210459a0504691cc0056a76512188dfd0ac2d9e3
Cheikh68 <sallcheikh222@gmail.com>
2026-09-19 12:55:23 -0400

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        count1 = defaultdict(int)
+        for char in s1:
+            count1[char] += 1
+        
+        i = 0
+        j = len(s1) - 1
+        count2 = defaultdict(int)
+        for char in s2[i:j+1]:
+            count2[char] += 1
+        print(f"initial count2 is {count2}")
+
+        def dicts_equal(a, b):
+            keys = a.keys() | b.keys()
+            return all(a.get(k, 0) == b.get(k, 0) for k in keys)
+
+        while j < len(s2):
+            if dicts_equal(count1, count2):
+                return True
+            else:
+                print(f"Comparison failed for {count1} and {count2}")
+                count2[s2[i]] -= 1
+                i += 1
+                j += 1
+
+                if j < len(s2):
+                    count2[s2[j]] += 1
+        
+        return False
